a48083c4dc021ba28920143b8582deae6f173bd4
Kevin Wessa <kjwessa@gmail.com>
2024-04-15 13:09:47 -0400

    feat: add initial movie data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,50 @@
+const tempMovieData = [
+  {
+    imdbID: "tt0468569",
+    Title: "The Dark Knight",
+    Year: "2008",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+  },
+  {
+    imdbID: "tt0107048",
+    Title: "Groundhog Day",
+    Year: "1993",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZWIxNzM5YzQtY2FmMS00Yjc3LWI1ZjUtNGVjMjMzZTIxZTIxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+  },
+  {
+    imdbID: "tt14544192",
+    Title: "Bo Burnham: Inside",
+    Year: "2021",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMWYwZTVlMTgtZWNkMS00OTk4LWI4YjMtMGNlNzA4OWIyOWU4XkEyXkFqcGdeQXVyNjYyMjE4NDY@._V1_.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt0468569",
+    Title: "The Dark Knight",
+    Year: "2008",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+    runtime: 152,
+    imdbRating: 9.0,
+    userRating: 9.9,
+  },
+  {
+    imdbID: "tt14544192",
+    Title: "Bo Burnham: Inside",
+    Year: "2021",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMWYwZTVlMTgtZWNkMS00OTk4LWI4YjMtMGNlNzA4OWIyOWU4XkEyXkFqcGdeQXVyNjYyMjE4NDY@._V1_.jpg",
+    runtime: 97,
+    imdbRating: 8.6,
+    userRating: 10,
+  },
+];
+
 export default function App() {
   return <></>;
 }
