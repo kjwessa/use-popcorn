b2f18c3d96ddd20cc2bdd916c81774f2029576f2
Kevin Wessa <kjwessa@gmail.com>
2024-06-16 15:33:24 -0400

    build(deps): add million.js for better React linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    MillionLint.vite(), // Ensure this returns a single plugin instance
+    react(), // Ensure this returns a single plugin instance
+  ],
   server: {
     open: true,
   },
