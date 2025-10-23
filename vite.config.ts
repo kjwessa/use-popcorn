73895b58142c746fd0754efa045f80e4a2308036
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2025-10-23 19:52:35 -0400

    chore (deps): update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    MillionLint.vite(), // Ensure this returns a single plugin instance
-    react(), // Ensure this returns a single plugin instance
-  ],
+  plugins: [react()],
   server: {
     open: true,
   },
